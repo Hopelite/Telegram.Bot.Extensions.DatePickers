8e095d193394106db0e5b01bad59ec8d5bb2675b
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-04-01 21:11:30 +0200

    feat: implement KeyboardDayOfMonthPicker, extract button factories to separate class

---
+++ src/Telegram.Bot.Extensions.DatePickers/Abstractions/IButtonFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Telegram.Bot.Extensions.DatePickers.Abstractions
+{
+    /// <summary>
+    /// Defines methods for date picking buttons factories.
+    /// </summary>
+    /// <typeparam name="T">Type of buttons.</typeparam>
+    public interface IButtonFactory<out T>
+    {
+        /// <summary>
+        /// Creates <typeparamref name="T"/> button of day of month picker.
+        /// </summary>
+        /// <param name="date"><see cref="DateTime"/> to create button with.</param>
+        /// <returns>Date picker button.</returns>
+        public T CreateButton(DateTime date);
+
+        /// <summary>
+        /// Creates empty <typeparamref name="T"/> button of day of month picker.
+        /// </summary>
+        /// <returns>Empty date picker button.</returns>
+        public T CreateEmptyButton();
+    }
+}
---
+++ src/Telegram.Bot.Extensions.DatePickers/Keyboard/KeyboardDayOfMonthPicker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Telegram.Bot.Extensions.DatePickers.Abstractions;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Bot.Extensions.DatePickers.Keyboard
+{
+    /// <summary>
+    /// Implements <see cref="DayOfMonthPicker{T}"/> with <see cref="KeyboardButton"/>s.
+    /// </summary>
+    public class KeyboardDayOfMonthPicker : DayOfMonthPicker<KeyboardButton>
+    {
+        private readonly IButtonFactory<KeyboardButton> _keyboardButtonFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyboardDayOfMonthPicker"/> with the first day of week set to Monday.
+        /// </summary>
+        /// <param name="date"><see cref="DateTime"/> with date which days to use.</param>
+        /// <param name="keyboardButtonFactory"><see cref="IButtonFactory{T}"/> to create buttons with.</param>
+        public KeyboardDayOfMonthPicker(DateTime date, IButtonFactory<KeyboardButton> keyboardButtonFactory)
+            : this(date, DayOfWeek.Monday, keyboardButtonFactory)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyboardDayOfMonthPicker"/> with the first day of week set to Monday.
+        /// </summary>
+        /// <param name="date"><see cref="DateTime"/> with date which days to use.</param>
+        /// <param name="weekBeginsWith"><see cref="DayOfWeek"/> from which week begins.</param>
+        /// <param name="keyboardButtonFactory"><see cref="IButtonFactory{T}"/> to create buttons with.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="keyboardButtonFactory"/> is null.</exception>
+        public KeyboardDayOfMonthPicker(DateTime date, DayOfWeek weekBeginsWith, IButtonFactory<KeyboardButton> keyboardButtonFactory)
+            : base(date, weekBeginsWith)
+        {
+            _keyboardButtonFactory = keyboardButtonFactory ?? throw new ArgumentNullException(nameof(keyboardButtonFactory));
+        }
+
+        /// <inheritdoc/>
+        protected override KeyboardButton CreateButton(DateTime date)
+        {
+            return _keyboardButtonFactory.CreateButton(date);
+        }
+
+        /// <inheritdoc/>
+        protected override KeyboardButton CreateEmptyButton()
+        {
+            return _keyboardButtonFactory.CreateEmptyButton();
+        }
+    }
+}
